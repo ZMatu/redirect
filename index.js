6aaa2f909f1a880ecb603d16afcc47d5c3c68a6a
Matias <matiasgenial2015@gmail.com>
2025-01-06 05:53:40 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const { ip, port } = req.query;
+
+  if (ip && port) {
+    const connectUrl = `steam://connect/${ip}:${port}`;
+    res.redirect(connectUrl);
+  } else {
+    res.status(400).send('Invalid address');
+  }
+});
+
+const PORT = 3000; // Cambia este puerto según lo necesites
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
